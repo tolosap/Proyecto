9ed92997100ed3e44d6b44b401239fc3f5a99af5
Victor Jurado <victorjc7@gmail.com>
2017-02-15 20:00:02 +0100

    Muuuchos cambios
    jshsjah

---
+++ Scripts/galeria.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $(".filter-button").click(function(){
+    // Para borrar la clase active que hace que el boton activo tenga unas propiedades CSS:
+    var botonActivo = $('button').filter('.active');
+    botonActivo.removeClass('active');
+    var value = $(this).attr('data-filter');
+    // Le ponemos las propiedades CSS al boton que vamos a activar:
+    var activo = $(this).addClass('active');
+    if(value == "all"){
+      $('.filter').show('1000');
+    }else{
+  $(".filter").not('.'+value).hide('3000');
+  $('.filter').filter('.'+value).show('3000');
+}
+});
+});
